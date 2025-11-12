fe121daae278760481e1d757f921349dd6523fe3
SpectraCode-Tech <dimejirsq08@gmail.com>
2025-11-12 16:53:30 +0100

    Nav Bar

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const menuClose = document.getElementById("menuClose");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.remove("translate-x-full");
+    mobileMenu.classList.add("translate-x-0");
+  });
+
+  menuClose.addEventListener("click", () => {
+    mobileMenu.classList.remove("translate-x-0");
+    mobileMenu.classList.add("translate-x-full");
+  });
+});
