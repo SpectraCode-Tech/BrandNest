973659f0b3ca535929f9379da7944511d1d79117
SpectraCode-Tech <dimejirsq08@gmail.com>
2025-11-12 17:00:31 +0100

    Made nav bar show

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenu = document.getElementById("mobileMenu");
 
   menuToggle.addEventListener("click", () => {
-    mobileMenu.classList.remove("translate-x-full");
-    mobileMenu.classList.add("translate-x-0");
+    mobileMenu.classList.remove("hidden");
   });
 
   menuClose.addEventListener("click", () => {
-    mobileMenu.classList.remove("translate-x-0");
-    mobileMenu.classList.add("translate-x-full");
+    mobileMenu.classList.add("hidden");
   });
 });
